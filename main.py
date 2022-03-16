f6a66f8b6318ffe1ce740ad2599cafcfcfcae149
Aviral Sharma <85646759+aviralsharma12@users.noreply.github.com>
2022-03-17 00:46:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import streamlit as st
-import pickle
+import pandas as pd
 
-User_list = pickle.load(open('User.pkl','rb'))
-users_list = User_list['id'].unique()
+User_list = pd.read_csv("retail_order_history.csv")
+users_list = User_list['user_id'].unique()
+user=User_list[["user_id","Age","gender","product_name"]].value_counts().reset_index()
+user.columns=['id',"age","Gen",'product','count']
 
 from sklearn.model_selection import train_test_split
-trainset, testset = train_test_split(User_list, test_size=0.25)
+trainset, testset = train_test_split(user, test_size=0.25)
 
 import turicreate as tc
 train_data = tc.SFrame(trainset)
 test_data = tc.SFrame(testset)
 item_sim_model =(tc.item_similarity_recommender.create(test_data, user_id='id', item_id='product', target='count', similarity_type='cosine'))
 
-primaryColor="#65b11b"
-backgroundColor="#b4d2c2"
-secondaryBackgroundColor="#8793c7"
-textColor="#181716"
-font="serif"
-
 
 st.title("Recommendation system")
 select_user_id = st.selectbox('Select User_Id ?',users_list)
 
-age=User_list['age'][User_list['id']==select_user_id].values[0]
+age=user['age'][user['id']==select_user_id].values[0]
 select_age = st.selectbox('selcted user_id age is',(age,0,0))
 
-gender=User_list['Gen'][User_list['id']==select_user_id].values[0]
+gender=user['Gen'][user['id']==select_user_id].values[0]
 select_gender = st.selectbox('Select user_id gender is',(gender,0,0))
 
 recommendation = item_sim_model.recommend(users=[select_user_id],k=3)
